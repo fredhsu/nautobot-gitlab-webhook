1dbd87d2ac895e90a39d938242dd8713df0370c8
Fred Hsu <fredlhsu@gmail.com>
2021-07-16 09:27:55 -0700

    Added unit test for permit from ips

--- src/avd.rs
+++ src/avd.rs
@@ -1,18 +1,18 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
 pub struct ExtendedACLs {
     pub r#type: String,
     pub access_lists: HashMap<String, ExtendedACL>,
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Clone, Debug, PartialEq)]
 pub struct ExtendedACL {
     pub sequence_numbers: HashMap<i32, AccessListEntry>,
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Clone, Debug, PartialEq)]
 pub struct AccessListEntry {
     pub action: String,
 }
@@ -45,8 +45,18 @@ mod tests {
         let ips = vec![crate::nautobot::IpAddressType {
             address: "10.1.1.0/24".to_owned(),
         }];
+        let ale = AccessListEntry {
+            action: "permit ip any 10.1.1.0/24".to_owned(),
+        };
+        let sequence_numbers: HashMap<i32, AccessListEntry> = [(10, ale)].iter().cloned().collect();
+        let eacl = ExtendedACL { sequence_numbers };
+        let access_lists: HashMap<String, ExtendedACL> =
+            [("critical".to_owned(), eacl)].iter().cloned().collect();
+        let acl = ExtendedACLs {
+            r#type: "l3leaf".to_owned(),
+            access_lists,
+        };
         let result = permit_from_ips(&ips);
-        assert_eq!(result.access_lists.len(), 1);
-        assert_eq!(result.r#type, "l3leaf");
+        assert_eq!(result, acl);
     }
 }
